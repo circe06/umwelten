b6a09398239ee69ef02fcc19b7cd6690d446d2ef
Pallavi Ray <pallavi.ray24@gmail.com>
2023-03-08 19:21:19 +0000

    Updated new theme

---
+++ wp-content/plugins/create-block-theme/admin/class-create-block-theme-admin.php
@@ -0,0 +1,6 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\wordpress\wp-content\plugins\create-block-theme\admin\resolver_additions.php:90
+Stack trace:
+#0 C:\xampp\htdocs\wordpress\wp-content\plugins\create-block-theme\admin\class-create-block-theme-admin.php(3): require_once()
+#1 {main}
+  thrown in <b>C:\xampp\htdocs\wordpress\wp-content\plugins\create-block-theme\admin\resolver_additions.php</b> on line <b>90</b><br>
---
+++ wp-content/plugins/create-block-theme/admin/resolver_additions.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\wordpress\wp-content\plugins\create-block-theme\admin\resolver_additions.php:90
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\wordpress\wp-content\plugins\create-block-theme\admin\resolver_additions.php</b> on line <b>90</b><br>
